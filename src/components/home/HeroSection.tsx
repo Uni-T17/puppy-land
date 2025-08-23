6090a0e21d5f41c0c9d19d3614ac3037a3b27155
Uni-T17 <mintayzamt17@gmail.com>
2025-08-24 00:49:08 +0700

    Done - Hero Section framework

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -3,20 +3,30 @@ import { Button } from "../ui/button";
 function HeroSection() {
   return (
     <>
-      <div className="">
-        <div className="">🎉Discover Your Perfect Companion</div>
-        <div className="">Learn About Puppies</div>
-        <p className="">
-          Discover how smart they are, how friendly with humans, and how to care
-          for them. Find the perfect puppy breed for your lifestyle with our
-          comprehensive guide.
-        </p>
+      <div className="flex items-center justify-around p-20">
+        <div className="flex flex-col gap-5">
+          <div className="w-fit rounded-xl bg-sky-100">
+            <p className="p-3 text-sm">🎉Discover Your Perfect Companion</p>
+          </div>
+
+          <div className="text-6xl font-extrabold">
+            <p className="">Learn About</p>
+            <p className="">Puppies</p>
+          </div>
+          <p className="text-muted-foreground w-100 text-sm">
+            Discover how smart they are, how friendly with humans, and how to
+            care for them. Find the perfect puppy breed for your lifestyle with
+            our comprehensive guide.
+          </p>
+          <div className="flex gap-4">
+            <Button className=""> 🔍 Expolre Breads </Button>
+            <Button className=""> 📝 Take Quiz </Button>
+          </div>
+        </div>
         <div className="">
-          <Button className=""> 🔍 Expolre Breads </Button>
-          <Button className=""> 📝 Take Quiz </Button>
+          <div className="h-100 w-180 bg-sky-200"></div>
         </div>
       </div>
-      <div className=""></div>
     </>
   );
 }
